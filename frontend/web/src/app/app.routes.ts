ec5330596180f51c8b3c2b3038c1999ca14b70be
Pierluigi Frascogna <frascogna.pierluigi@gmail.com>
2025-12-07 22:17:47 +0100

    chore [main]: changed project structure

--- frontend/web/src/app/app.routes.ts
+++ frontend/web/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { Login } from './auth/login/login';
-import { Home } from './home/home';
-import { Profile } from './profile/profile';
+import { Login } from './core/auth/login/login';
+import { Home } from './modules/home/home';
+import { Profile } from './modules/profile/profile';
 import { authGuard } from './auth/auth-guard';
-import { noAuthGuard } from './auth/no-auth-guard';
+import { noAuthGuard } from './core/auth/no-auth-guard';
 
 
 export const routes: Routes = [
