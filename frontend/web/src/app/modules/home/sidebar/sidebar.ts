3628530ff8049628ea20e92f522cd92cfd41e35d
4lePl4y <aletheplayer2@gmail.com>
2025-12-10 11:37:50 +0100

    chore [main]: changed sidebar for a more efficient use of space

--- frontend/web/src/app/modules/home/sidebar/sidebar.ts
+++ frontend/web/src/app/modules/home/sidebar/sidebar.ts
@@ -1,25 +1,37 @@
 import { Component, inject } from '@angular/core';
 import { NgClass } from '@angular/common';
-import { ProjectStore } from './project/project-store';
+import { ProjectStore } from './sidebar-element/project/project-store';
+import { Project } from './sidebar-element/project/project-model';
+import { SidebarElement } from './sidebar-element/sidebar-element';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [NgClass],
+  imports: [NgClass, SidebarElement],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
   
-  private projectsStore = inject(ProjectStore);
+  //private projectsStore = inject(ProjectStore);
+  readonly projectsStore = [
+    {
+      uuid: "abcde",
+      name: "Backend",
+      creation_date: new Date(),
+    } as Project,
 
+    {
+      uuid: "fghi",
+      name: "Frontend dsdaassdasdasd",
+      creation_date: new Date(),
+    }as Project
+
+  ]
   isSidebarOpen: boolean = true; 
 
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
 
-  constructor() {
-    console.log("provaprovapeova", this.projectsStore.projects());
-  }
-
+  
 }
