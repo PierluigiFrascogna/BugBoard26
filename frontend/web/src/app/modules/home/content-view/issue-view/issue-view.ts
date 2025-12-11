cec28885e1541ed207c221ff7de635f616d9f148
4lePl4y <aletheplayer2@gmail.com>
2025-12-12 00:04:52 +0100

    imp [main]: issue-view first draft
    prima implementazione di issue-view, che come issue-list si occupa di creare dei componenti, gli issue-event-card. Ha un funzionamento del tutto simile a issue-list.
    issue-list : issue-card = issue-view : issue-event-card

---
+++ frontend/web/src/app/modules/home/content-view/issue-view/issue-view.ts
@@ -0,0 +1,29 @@
+import { Component, computed, inject, Signal } from '@angular/core';
+import { ProjectStore } from '../../sidebar/sidebar-element/project/project-store';
+import { IssueStore } from '../issues-list/issue-card/issue/issue-store';
+import { IssueEventStore } from './issue-event-card/issue-events/issue-event-store';
+import { IssueEventCard } from "./issue-event-card/issue-event-card";
+import { IssueEvent } from './issue-event-card/issue-events/issue-event';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-issue-view',
+  imports: [IssueEventCard, NgClass],
+  templateUrl: './issue-view.html',
+  styleUrl: './issue-view.css',
+})
+export class IssueView {
+
+  private readonly projectStore = inject(ProjectStore);
+  private readonly issueStore = inject(IssueStore);
+  private readonly issueEventStore = inject(IssueEventStore);
+
+  readonly title: Signal<string> = computed(() => this.projectStore.name()+this.issueStore.title());
+  readonly issueEvents: Signal<IssueEvent[]> = computed(() => this.issueEventStore.issueEvents());
+  readonly isVisible: Signal<boolean> = computed(() => this.issueStore.selectedIssue()!==null);
+
+  deselectIssue(){
+    this.issueStore.deselectIssue();
+  }
+  
+}
