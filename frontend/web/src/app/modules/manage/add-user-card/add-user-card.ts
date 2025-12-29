79f93206972a313353fc0d1a3f4c416d773024b9
4lePl4y <aletheplayer2@gmail.com>
2025-12-29 18:06:51 +0100

    dev [main]: added functionality to add an user for admin

---
+++ frontend/web/src/app/modules/manage/add-user-card/add-user-card.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from "@angular/forms";
+import { UserStore } from '../../profile/user/user-store';
+
+@Component({
+  selector: 'app-add-user-card',
+  imports: [ReactiveFormsModule],
+  templateUrl: './add-user-card.html',
+  styleUrl: './add-user-card.css',
+})
+export class AddUserCard {
+  private readonly userStore = inject(UserStore);
+
+  userForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    surname: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    role: new FormControl('', [Validators.required])
+  });
+
+
+  createUser(){
+    if(!this.userForm.valid)
+      return;
+    else{
+      //TODO: chiamata alla user store per inserire la nuova utenza
+    }
+  }
+}
