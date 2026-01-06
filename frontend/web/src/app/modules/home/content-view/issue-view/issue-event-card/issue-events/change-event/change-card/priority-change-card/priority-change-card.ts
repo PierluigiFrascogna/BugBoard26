f2d4f800f618edd8fd6b282a9f6b49c113feff5f
4lePl4y <aletheplayer2@gmail.com>
2026-01-07 00:26:30 +0100

    chore [main]: beautification of changes-cards

--- frontend/web/src/app/modules/home/content-view/issue-view/issue-event-card/issue-events/change-event/change-card/priority-change-card/priority-change-card.ts
+++ frontend/web/src/app/modules/home/content-view/issue-view/issue-event-card/issue-events/change-event/change-card/priority-change-card/priority-change-card.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { PriorityChange } from '../change/priority-change';
 import { DatePipe } from '@angular/common';
+import { PriorityLabel } from "../../../../../../issues-list/issue-card/priority-label/priority-label";
 
 @Component({
   selector: 'app-priority-change-card',
-  imports: [DatePipe],
+  imports: [DatePipe, PriorityLabel],
   templateUrl: './priority-change-card.html',
   styleUrl: './priority-change-card.css',
 })
--- frontend/web/src/app/modules/home/content-view/issue-view/issue-event-card/issue-events/change-event/change-card/state-change-card/state-change-card.ts
+++ frontend/web/src/app/modules/home/content-view/issue-view/issue-event-card/issue-events/change-event/change-card/state-change-card/state-change-card.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { StateChange } from '../change/state-change';
 import { DatePipe } from '@angular/common';
+import { StateLabel } from "../../../../../../issues-list/issue-card/state-label/state-label";
 
 @Component({
   selector: 'app-state-change-card',
-  imports: [DatePipe],
+  imports: [DatePipe, StateLabel],
   templateUrl: './state-change-card.html',
   styleUrl: './state-change-card.css',
 })
