65ca1214d845e762e158cb91c5a3517730602231
4lePl4y <aletheplayer2@gmail.com>
2026-01-07 11:46:24 +0100

    fix [main]: correcting the post comment api url
    (avevo messo due volte l'issue uuid)

--- frontend/web/src/app/modules/home/content-view/issue-view/issue-event-card/issue-events/issue-event-api.ts
+++ frontend/web/src/app/modules/home/content-view/issue-view/issue-event-card/issue-events/issue-event-api.ts
@@ -35,7 +35,7 @@ export class IssueEventApi {
 
   createComment(comment: Comment["text"]){
     return this.http.post<Comment>(
-      `${this.API_URL}${this.ISSUES_URL}/${this.issueStore.selectedIssue()?.uuid}/${this.issueStore.selectedIssue()?.uuid}${this.COMMENTS_URL}`, comment
+      `${this.API_URL}${this.ISSUES_URL}/${this.projectStore.selectedProject()?.uuid}/${this.issueStore.selectedIssue()?.uuid}${this.COMMENTS_URL}`, comment
     );
   }
 }
