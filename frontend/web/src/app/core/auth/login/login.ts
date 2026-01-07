62941f6af366ff5855402990d136917c32ca6233
4lePl4y <aletheplayer2@gmail.com>
2026-01-07 14:54:10 +0100

    Update login.ts

--- frontend/web/src/app/core/auth/login/login.ts
+++ frontend/web/src/app/core/auth/login/login.ts
@@ -25,7 +25,7 @@ export class Login {
 
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    password: new FormControl('', [Validators.required])
   });
 
   onSubmit() {
