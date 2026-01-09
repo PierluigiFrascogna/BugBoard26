8dc0a3d4af030ec3728b4c6a6a497324ac1a1be8
Marco33-dev <marcofesta105@gmail.com>
2026-01-10 00:01:25 +0100

    Update BugboardApplicationTests.java
    L'ultimo commit faceva fallire una github action in quanto la funzione di test conteneva un errore di compilazione

--- backend/bugboard/src/test/java/it/bugboard26/bugboard/BugboardApplicationTests.java
+++ backend/bugboard/src/test/java/it/bugboard26/bugboard/BugboardApplicationTests.java
@@ -3,23 +3,14 @@ package it.bugboard26.bugboard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import it.bugboard26.bugboard.modules.auth.JwtService;
-import it.bugboard26.bugboard.modules.projects.HeaderRequestService;
-
-import static org.mockito.Mockito.*;
-
 @SpringBootTest
 class BugboardApplicationTests {
-	
-	HeaderRequestService mockedHeaderRequestService = mock(HeaderRequestService.class);
-	JwtService mockedJwtService = mock(JwtService.class);
-	
-	
+
 	@Test
-	void UpdateIssueWithViewer() {
-		when(mockedHeaderRequestService.hasAuthorization()).thenReturn(true);
-		when(mockedJwtService.parseToken("fakeRawToken")).thenReturn();
-		
+	void contextLoads() {
+
 	}
 
+
 }
+
