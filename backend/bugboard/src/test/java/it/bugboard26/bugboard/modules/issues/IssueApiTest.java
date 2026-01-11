9685fd82707285c8a06fe5a820c80eb80109bfb8
Pierluigi Frascogna <frascogna.pierluigi@gmail.com>
2026-01-11 20:58:14 +0100

    test [main]: added test case 6

--- backend/bugboard/src/test/java/it/bugboard26/bugboard/modules/issues/IssueApiTest.java
+++ backend/bugboard/src/test/java/it/bugboard26/bugboard/modules/issues/IssueApiTest.java
@@ -5,20 +5,43 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import it.bugboard26.bugboard.modules.auth.JwtService;
+import it.bugboard26.bugboard.modules.projects.HeaderRequestService;
+import it.bugboard26.bugboard.modules.users.UserService;
 
 @ExtendWith(MockitoExtension.class)
 class IssueApiTest {
 
-    @Mock
-    private JwtService jwtService;
-
     @InjectMocks
     private IssueApi issueApi;
 
-    @Test
-    void getIssuesByProject_input_output() {
+    @Mock
+    private HeaderRequestService headerRequest;
 
+    @Mock
+    private JwtService jwtService;
+
+    @Mock
+    private IssueService issueService;
+
+    @Mock
+    private UserService userService;
+
+    @Test
+    void getIssuesByProject_TC6() {
+
+        when(headerRequest.hasAuthorization()).thenReturn(false);
+
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+            issueApi.getIssuesByProject(null, null, null, null);
+        });
+        assertEquals(HttpStatus.UNAUTHORIZED, exception.getStatusCode());
+        
     }
 }
